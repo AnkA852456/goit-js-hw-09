fe595c773a305ccfcad2d212ee4802a6c42ae705
AnkA852456 <108530192+AnkA852456@users.noreply.github.com>
2022-11-24 10:38:31 +0200

    HW9_task_1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const refs = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
 
+let intervalId = null;
+
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+  intervalId = setInterval(makeChangeColor, 1000);
+  refs.startBtn.disabled = true;
+}
+
+function onStopBtnClick() {
+  clearInterval(intervalId);
+  refs.startBtn.disabled = false;
+}
+
+function makeChangeColor() {
+  const colorRandom = getRandomHexColor();
+  refs.body.style.backgroundColor = colorRandom;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
